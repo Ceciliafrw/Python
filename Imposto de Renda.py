3f32250e672b6876d1f10467f427671c873fa215
FERREIRA <cecilia.ferreira@vidanova.local>
2022-05-16 15:21:39 -0300

    Adicionando Primeiro Exercicio em Python - Calculo de Imposto de Renda.py

--- Imposto de Renda.py
+++ Imposto de Renda.py
@@ -11,12 +11,12 @@ If salario >= 1903.99 and <= 2826.65
 #De R$ 2.826,66 a R$ 3.751,05 = 15% de IR
     elif salario >= 2826.66 and  <=3751.05
     salarioliq = salario * 85
-        Print ("Salario com desconto de 15% de IR " ,salarioliq)
+    Print ("Salario com desconto de 15% de IR " ,salarioliq)
 
 #De R$ 3.751,06 a R$ 4.664,68 = 22,5% de IR
     elif salario >= 3751.06 and  <=4664.68
     salarioliq = salario * 77.5
-        Print ("Salario com desconto de 22,5% de IR " ,salarioliq)
+    Print ("Salario com desconto de 22,5% de IR " ,salarioliq)
 
 #Maior que  R$ 4664,68 = 27,5% de IR
     elif salario >= 4664.68
@@ -24,6 +24,7 @@ If salario >= 1903.99 and <= 2826.65
     Print ("Salario com desconto de 15% de IR " ,salarioliq)
 
 #Salario menor do que R$ 1.903,98 - Isento
-else print ("Salario Isento de IR")
+else 
+print ("Salario Isento de IR")
 
 
