3e4e5b31e90fabc8b6847c93f9fa3f65a5c4bd6e
FERREIRA <cecilia.ferreira@vidanova.local>
2022-06-06 10:28:07 -0300

    Conclusão Sala de Cinema

--- FibonacciWhile.py
+++ FibonacciWhile.py
@@ -1,23 +1,27 @@
+Qnt = 1
+
+while (Qnt > 0):
 #Quantidade de vezes que vai rodar Fibonacci
-Qnt = int(input("Digite a quantidade de vezes para calculo do Fibonacci : "))
+    Qnt = int(input("Digite a quantidade de vezes para calculo do Fibonacci : "))
 
 #contar as vezes que rodou
-contador = 1
+    contador = 1
 
-#variaveis inicial para calcular fibonacci
-Primeironum = 0
-Segundonum = 1
+    #variaveis inicial para calcular fibonacci
+    Primeironum = 0
+    Segundonum = 1
 
-#imprimir as primeiras variaveis
-print( Primeironum)
-print( Segundonum )
-
-#calcular o fibonacci
-while (contador < Qnt):
-    Soma = Primeironum + Segundonum
-    Primeironum = Segundonum
-    Segundonum = Soma
-    print(Soma)
-    contador += 1
+    #imprimir as primeiras variaveis
+        
+        #print( Segundonum )
+    print( Primeironum )
+        #calcular o fibonacci
+    while (contador < Qnt):
+        
+        Soma = Primeironum + Segundonum
+        Primeironum = Segundonum
+        Segundonum = Soma
+        print(Soma)
+        contador += 1
 
 
--- SalaCinema.py
+++ SalaCinema.py
@@ -1,32 +1,64 @@
 #Montando uma sala de cinema
 
-from ast import While
+from itertools import count
+
+
+def split_list(lst,l):
+    for n in range(0, len(lst), l):
+        yield lst[n:n + l]
+
+l = 10
+
 def LugaresDisponiveis():
     global poltronareservada
-    poltronareservada = int(input("Selecione a poltrona desejada: "))-1
+    global totallugares
+    poltronareservada = int(input("\nSelecione a poltrona desejada: "))-1
+    #print(totallugares)
 
-    sala[poltronareservada] = ("x")
-    print(sala)
+    if sala[poltronareservada] != ("0"):
+        sala[poltronareservada] = ("0")
+        totallugares = int(totallugares) - 1
+        print("Poltrona" ,poltronareservada+1, "reservada\n")
 
+    elif totallugares == (0):
+        print("\nSem poltronas disponíveis, sessão lotada.\n")
+    
+    else:
+        print("\nPoltrona reservada, escolha outra \n")
+
+    return
+
+def cinema():
+    global fileiras
+    global coluna
+    fileira = list(split_list(sala, l))
+    print(coluna)
+    for fileiras in fileira:
+        print(fileiras)
+    
 sala = []
+coluna = ["A  B  C  D  E  F  G  H  I  J "]
 
 Qntdelugares = int(input("Informe a quantidade de lugares: ") )
 
-if Qntdelugares  == 0 :
-    print("Quantidade de lugares insuficiente para iniciar uma sala")
-    
-else: 
+while (Qntdelugares) == 0 :
+    print("Quantidade de lugares insuficiente para iniciar uma sala - REINICIE A SESSÃO")
+    Qntdelugares = int(input("Informe a quantidade de lugares: ") )
+
+else:
+
     for i in range(Qntdelugares):
-        sala.append(i+1) 
-print(sala)
-LugaresDisponiveis()
+            sala.append(i+1)
+            totallugares = len(sala)
 
-while True:
-    if  Qntdelugares > len(sala):
-        print("Sem poltronas disponíveis, sessão lotada.")
-        
-    else:
-        sala[poltronareservada] = sala[poltronareservada] == ("x")
-        print("Poltrona já reservada")
+    while (totallugares) >= 0 :
+        cinema()
         LugaresDisponiveis()
+        if  totallugares == (0):
+            print("\nSem poltronas disponíveis, sessão lotada.\n")
+            cinema()
+            print("O Filme já vai começar\n")
+            break
 
+
+        
