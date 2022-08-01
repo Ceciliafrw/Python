6a0f62e1c97d00d32b739423bbe9e289c8f11b02
FERREIRA <cecilia.ferreira@vidanova.local>
2022-08-01 19:25:47 -0300

    Atualização exercicio palavras

--- Palavra.py
+++ Palavra.py
@@ -5,11 +5,11 @@
     ##xponteiro.write(nome)
     ##xponteiro.write("\n")
 
-#ok -  Conte quantas palavras há no arquivo
-#ok -  Conte quantas vezes cada letra do alfabeto aparece no arquivo
-#ok -  Conte quantas palavras começam com cada letra do alfabeto
-#Identifique as palavras começam com as mesmas 3 letras do seu nome e salve-as num arquivo separado
-#Identifique as palavras que possuem todas as letras do seu nome e salve-as num arquivo separado
+#ok  Conte quantas palavras há no arquivo
+#ok Conte quantas vezes cada letra do alfabeto aparece no arquivo
+#ok Conte quantas palavras começam com cada letra do alfabeto
+#ok Identifique as palavras começam com as mesmas 3 letras do seu nome e salve-as num arquivo separado
+#ok Identifique as palavras que possuem todas as letras do seu nome e salve-as num arquivo separado
 #Identifique todas as palavras que são palíndromos e salve-as num arquivo separado
 
 #passando o nome do arquivo que deseja que seja aberto
@@ -41,7 +41,7 @@ def contar_letras_alfabeto(contar):
                 dic_letras_alfabeto[letra] = 1
     return dic_letras_alfabeto
 
-#contando as letra iniciais
+
 def contar_letras_inicio(contar):
     dic_letras_inicial= {}
     for palavra in contar:
@@ -51,27 +51,35 @@ def contar_letras_inicio(contar):
                 dic_letras_inicial[palavra[0]] = 1
     return dic_letras_inicial
 
-#contando quantas palavras tem a inicial do meu nome
-def contar_letras_meu_nome(contar):
-    dic_letras_inicial_meu_nome= {"CEC" : 0}
-    for palavra in contar:
-        if palavra[0] in dic_letras_inicial_meu_nome:
-                dic_letras_inicial_meu_nome[palavra[0]] += 1
 
-    return dic_letras_inicial_meu_nome
+def contar_letras_inicio_meu_nome(contar, iniciais):
+    with open('Iniciais Meu nome.txt', 'a') as meu_arquivo:
+         for palavra in contar:
+            if palavra[:3] == iniciais:
+                meu_arquivo.write(palavra)
+
+def contar_letras_meu_nome(contar):
+    meu_nome = ['C','E','L','I','A']
+    with open('Meu nome completo.txt', 'a') as arquivo_nome:
+        for letra in meu_nome:
+            for palavra in contar:
+                if palavra[0] == letra:
+                    arquivo_nome.write(palavra)
+                    arquivo_nome.write("\n")
+
 
 #imprimindo os resultados
+contar_letras_inicio_meu_nome(contar, iniciais="CEC")
+contar_letras_meu_nome(contar)
 qnt_palavras = contar_todas_palavras(contar)
 qnt_letras = contar_letras_alfabeto(contar)
-qnt_letras_inicial_meu_nome = contar_letras_meu_nome(contar)
 qnt_letras_inicial = contar_letras_inicio(contar)
 print("Eu consegui contar as", qnt_palavras , "palavras via Python\n")
-print("As letras do meu nome aparecem:", qnt_letras_inicial_meu_nome)
-print("A quantidade de cada letras é:\n")
+print("\nA quantidade de cada letras é:\n")
 for letra in qnt_letras:
     print(letra, ":",qnt_letras[letra])
 
-print("O acumulado de letras é:\n")
+print("\nO acumulado de letras é:\n")
 for letra in qnt_letras_inicial:
     print(letra, ":",qnt_letras_inicial[letra])
 
